3745926065a6e3652ee56d95314c6fad38d5159e
irdinamasturina <leo11249804@gmail.com>
2021-02-10 13:51:01 +0700

    10/02/2021

---
+++ Program 9.py
@@ -0,0 +1,12 @@
+num = int(input('Bilangan : '))
+
+faktorial = 1
+
+if num < 0:
+    print('Maaf, faktorial tidak bisa untuk bilangan negatif')
+elif num == 0:
+    print('Faktorial dari 0 adalah 1')
+else:
+    for i in range(1,num+1):
+        faktorial = faktorial*i
+    print('Faktorial dari',num,'adalah',faktorial)
