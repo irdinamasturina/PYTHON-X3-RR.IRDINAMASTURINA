250e6b6f88ba9bb2fab187112bc9ae51620972a7
irdinamasturina <leo11249804@gmail.com>
2021-02-24 13:58:40 +0700

    24/02/2021

--- Latihan Pemrograman 24 Februari/Program 1.py
+++ Latihan Pemrograman 24 Februari/Program 1.py
@@ -2,7 +2,7 @@ num = 407
 if num > 1:
     for i in range(2, num):
         if (num % i) == 0:
-            print(num,'bukan bilangan prima')
+            print(num, 'bukan bilangan prima')
             print(i,'kali',num//i,'adalah',num)
             break
     else:
