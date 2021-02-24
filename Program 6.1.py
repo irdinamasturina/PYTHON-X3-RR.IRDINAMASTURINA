55ed8997058fa8b38a0531df31aeee45c7fc77ad
irdinamasturina <leo11249804@gmail.com>
2021-02-24 13:41:12 +0700

    24/02/2021

---
+++ Program 6.1.py
@@ -0,0 +1,9 @@
+kata = 'nama saya sopan setiawan'
+
+jum = 0
+for letter in kata:
+    if letter == 's':
+        jum += 1
+        #continue
+    #print("huruf sekarang :",letter)
+print('Jumlah :',jum)
